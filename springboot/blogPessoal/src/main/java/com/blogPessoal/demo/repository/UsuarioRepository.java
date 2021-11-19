e32841ee6d092bd4b7607b7d3541d2dd74b466fc
azfernanda <fernanda.azoubel@gmail.com>
2021-11-19 13:07:00 -0300

    Security

---
+++ springboot/blogPessoal/src/main/java/com/blogPessoal/demo/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.blogPessoal.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blogPessoal.demo.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
